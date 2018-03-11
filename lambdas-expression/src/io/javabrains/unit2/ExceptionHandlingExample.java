715a23771b4a16a08c5edd1215197d2244fc2f94
Pankaj Soni <pankajsoni00100@gmail.com>
2018-03-12 00:51:17 +0530

    Before Till Exception Handling

--- lambdas-expression/src/io/javabrains/unit2/ExceptionHandlingExample.java
+++ lambdas-expression/src/io/javabrains/unit2/ExceptionHandlingExample.java
@@ -1,7 +1,55 @@
 package io.javabrains.unit2;
 
+import java.util.function.BiConsumer;
+
 /**
  * @author pankaj
  */
 public class ExceptionHandlingExample {
+
+    public static void main(String[] args) {
+        int [] someNumbers = {1,2,3,4};
+        int key = 0;
+
+//        process(someNumbers,0);
+//        process(someNumbers,key, ( v, k )-> System.out.println(v + k));
+
+        /* IF i pass key = 0 then it will throw Arithmetic Exception */
+        process(someNumbers, key, ( v, k ) -> System.out.println(v / k));
+
+
+        /*1) Handling Exception in method */
+        processHandle(someNumbers, key, ( v, k ) -> System.out.println(v / k));
+
+        /*1) Handling Exception in method in lambda expression it self */
+        processHandle(someNumbers, key, ( v, k ) ->{
+            try{
+                System.out.println(v / k))
+            }catch (ArithmeticException e){
+                e.printStackTrace();
+            }
+        } );
+
+        
+    }
+
+    private static void process(int[] someNumbers, int key) {
+        for (int number : someNumbers){
+            System.out.println(number + key);
+        }
+    }
+
+    private static void process(int[] someNumbers, int key, BiConsumer<Integer, Integer> consumer) {
+        for (int number : someNumbers){
+            System.out.println();
+            consumer.accept(  number ,key);
+        }
+    }
+
+    private static void processHandle(int[] someNumbers, int key, BiConsumer<Integer, Integer> consumer) {
+        for (int number : someNumbers){
+            System.out.println();
+            consumer.accept(  number ,key);
+        }
+    }
 }
